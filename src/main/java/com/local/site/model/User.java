0c0e6a134cf8610d2961f83384b9847fe979d994
domestos <elbarto2006@ukr.net>
2015-08-18 00:53:01 +0300

    added application Context added persistence (realized  connect on  database ( mysql)

---
+++ src/main/java/com/local/site/model/User.java
@@ -0,0 +1,25 @@
+package com.local.site.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+}
